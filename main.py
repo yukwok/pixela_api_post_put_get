71222c23b778730deb6cf450f1f295f78eb1e541
Yukwok <billynyk@gmail.com>
2021-06-21 16:39:05 +0800

    create graph, use HTTP header for auth-tication.

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import requests as requests
 
-url_create_user = "https://pixe.la/v1/users"
+USERNAME = "yukwok"
+TOKEN = "234ghsf97shfkhsjfksdfsdfswrsfdf"
+
+# USERNAME = "yukwokshanghai"
+# TOKEN = "234ghsf97shfkhsjfksdfsdfswrsfdf"
+
+PIXELA_URL = "https://pixe.la/v1"
+CREATE_USER_ENDPOINT = "/users"
+CREATE_GRAPH_ENDPOINT = f"/users/{USERNAME}/graphs"
 
 params_create_user = {
-    "token": "234ghsf97shfkhsjfksdfsdfswrsfdf",
-    "username": "yukwok",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
     }
 
+# X-USER-TOKEN:thisissecret
+headers = {
+    "X-USER-TOKEN":TOKEN
+}
+
+params_create_graph = {
+    "id": "graph1",
+    "name": "Physical exercises - running",
+    "unit": "km",
+    "type": "float",
+    "color": "sora",
+}
+
 # --- create user ---
-# response = requests.post(url=url_create_user, json=params_create_user)
-print(response.json())
+# response = requests.post(url=f"{PIXELA_URL}{CREATE_USER_ENDPOINT}", json=params_create_user)
+
+# --- create graph ----
+response = requests.post(url=f"{PIXELA_URL}{CREATE_GRAPH_ENDPOINT}", json=params_create_graph, headers=headers)
+print(response.text)
