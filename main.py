b97d8dd108402580b79bb925caa36640f3676cea
Yukwok <billynyk@gmail.com>
2021-06-21 17:09:17 +0800

    pixel added for today running distance

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 import requests as requests
 
 USERNAME = "yukwok"
 TOKEN = "234ghsf97shfkhsjfksdfsdfswrsfdf"
+GRAPH_ID = "/graph1"
 
 # USERNAME = "yukwokshanghai"
 # TOKEN = "234ghsf97shfkhsjfksdfsdfswrsfdf"
@@ -15,11 +18,11 @@ params_create_user = {
     "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
-    }
+}
 
 # X-USER-TOKEN:thisissecret
 headers = {
-    "X-USER-TOKEN":TOKEN
+    "X-USER-TOKEN": TOKEN
 }
 
 params_create_graph = {
@@ -34,5 +37,22 @@ params_create_graph = {
 # response = requests.post(url=f"{PIXELA_URL}{CREATE_USER_ENDPOINT}", json=params_create_user)
 
 # --- create graph ----
-response = requests.post(url=f"{PIXELA_URL}{CREATE_GRAPH_ENDPOINT}", json=params_create_graph, headers=headers)
+# response = requests.post(url=f"{PIXELA_URL}{CREATE_GRAPH_ENDPOINT}", json=params_create_graph, headers=headers)
+# print(response.text)
+
+# --- create a pixel
+today = datetime.today().strftime('%Y%m%d')
+
+create_pixel_endpoint = f"{PIXELA_URL}{CREATE_USER_ENDPOINT}/{USERNAME}/graphs{GRAPH_ID}"
+
+print(create_pixel_endpoint)
+
+graph_data = {
+    "date": today,
+    "quantity": "4.4"  # 4.4 km
+}
+
+response = requests.post(url=create_pixel_endpoint, json=graph_data, headers=headers)
 print(response.text)
+
+
